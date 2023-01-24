a17b3f306534b992e288505822cd56162a41402c
iivan <iivanov.e2018@gmail.com>
2023-01-24 20:59:07 +0300

    add functional menu bar

--- src/dsg/Forms/MainForm.py
+++ src/dsg/Forms/MainForm.py
@@ -1,6 +1,6 @@
-from PySide2.QtWidgets import QMainWindow, QStackedLayout, QSizePolicy
+from PySide2.QtWidgets import QMainWindow
 from src.dsg.uicode.Main_Form import Ui_MainWindow
-from src.dsg.Forms.Schedule_show import ShowSchedule
+
 
 
 class MainForm(QMainWindow, Ui_MainWindow):
@@ -8,15 +8,17 @@ class MainForm(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.showFullScreen()
         self.show_schdl_act.triggered.connect(self.show_schdl)
         self.edit_schdl_act.triggered.connect(self.show_edit)
         self.write_srvc_act.triggered.connect(self.write_on_service)
         self.schdl_srvc_act.triggered.connect(self.show_schdl_service)
         self.profile_act.triggered.connect(self.show_profile)
         self.exit_act.triggered.connect(self.close)
+        self.begin_work.clicked.connect(self.show_schdl)
 
     def show_schdl(self):
-       self.stackedWidget.setCurrentIndex(1)
+        self.stackedWidget.setCurrentIndex(1)
 
     def show_edit(self):
         self.stackedWidget.setCurrentIndex(2)
